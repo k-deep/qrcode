de53db7d082e95e5af25ff0b7a4418c442092050
k-deep <karandeep@gmail.com>
2013-12-20 10:23:39 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 document.addEventListener("deviceready",function()
 {
-	//var scanner = cordova.require("cordova/plugin/BarcodeScanner");
-	cordova.plugins.barcodeScanner.scan(
+	var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+	//cordova.plugins.barcodeScanner
+	scanner.scan(
 	function (result) {
 		$("#results").html("We got a barcode\n" + "Result: " + result.text + "\n" + "Format: " + result.format + "\n" + "Cancelled: " + result.cancelled);
 	}, 
