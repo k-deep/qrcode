a7f76811c3f9a7ee21cbeef39789c93e198e8337
k-deep <karandeep@gmail.com>
2013-12-19 23:15:46 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+$(function (){
+	cordova.plugins.barcodeScanner.scan(
+	function (result) {
+		$("#results").html("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+	}, 
+	function (error) {
+		$("#results").html("Scanning failed: " + error);
+	});
+});
